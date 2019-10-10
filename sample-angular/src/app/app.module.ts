438f2843d0b32a1b4b507ae2736df64de06e9d4d
Ryan Fiegle <rpfiegle@gmail.com>
2019-10-09 23:23:17 -0400

    angular module and service

--- sample-angular/src/app/app.module.ts
+++ sample-angular/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+
+import { EnvironmentUrlService } from './shared/services/environment-url.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    EnvironmentUrlService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
